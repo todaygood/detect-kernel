5606e725fc7ca25675a2328662914a7a3c3c341b
margin <pcentos@163.com>
2021-01-01 16:22:06 +0800

    first

---
+++ config/detect-kernel-config.go
@@ -0,0 +1,34 @@
+package main 
+
+import (
+  "golang.org/x/sys/unix"
+  "os"
+  "fmt"
+  "bytes"
+  "strings"
+)
+
+func main() {
+    // Check Kernel Config is available or not.
+    // We are replicating BPFTools logic here to check if kernel config is available
+    // https://elixir.bootlin.com/linux/v5.7/source/tools/bpf/bpftool/feature.c#L390
+    info := unix.Utsname{}
+    err := unix.Uname(&info)
+    if err != nil {
+       fmt.Println("unix.Uname error")
+    }
+	  
+    release := strings.TrimSpace(string(bytes.Trim(info.Release[:], "\x00")))
+   
+   fmt.Println("release:"+release)
+
+    // Any error checking these files will return Kernel config not found error
+    if _, err := os.Stat(fmt.Sprintf("/boot/config-%s", release)); err != nil {
+        if _, err = os.Stat("/proc/config.gz"); err != nil {
+       	     fmt.Println("kernel config is not found")
+        }
+    }
+
+    fmt.Println("kernel config has been found")
+}
+
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/todaygood/detect-kernel
+
+go 1.15
+
+require golang.org/x/sys v0.0.0-20201231184435-2d18734c6014
